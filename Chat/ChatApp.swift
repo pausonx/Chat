dd1c6a0f6c491e2f3dc1a1772b7125b5e20398dd
Paulina Wyskiel <p.wyskiel@outlook.com>
2023-04-14 11:07:59 +0200

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Paulina Wyskiel on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
